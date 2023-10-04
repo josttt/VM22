6791b814e805111d764b82fbd436cd1c4be9f0fd
Jost Tristan Toomis <josttristan.toomis@tptlive.ee>
2023-10-04 14:10:17 +0300

    suema

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 let array = [1, 2, true, 1.2, 'hello'];
 console.log(array);
+console.log(array[4]);
+
+array[3] = 69;
+console.log(array);
+array.push('new value')
+console.log(array);
+array = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+console.log(array);
+console.log(array[1][2]);
+
+let object = {
+    name: 'tyrone',
+    age: 12,
+    pronouns: ['nig', 'ga']
+};
+console.log(object);
+console.log(object.pronouns);
+console.log(console);
