4c7fcf5ce06b67f0ec7a5bdc245b97640a97e3d3
Jost Tristan Toomis <josttristan.toomis@tptlive.ee>
2023-10-04 13:40:42 +0300

    su ema

---
+++ script.js
@@ -0,0 +1,11 @@
+let boolean = true;
+console.log(boolean);
+boolean = false;
+console.log(boolean);
+boolean = true && false && true;
+console.log(boolean);
+
+num = 10
+if(num == 10) {
+    console.log('võrdne kümnega')
+}
