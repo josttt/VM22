8bdcedfab1663487418183f2fadf4e7667de4688
josttt <josttristan.toomis@tptlive.ee>
2023-10-11 13:19:31 +0300

    ye

--- script.js
+++ script.js
@@ -21,4 +21,27 @@ let object = {
 };
 console.log(object);
 console.log(object.pronouns);
-console.log(console);
+
+for(let i=0; i<10; i++){
+    console.log(i);
+}
+let i = 1;
+while(i<1_000_000){
+    console.log(i);
+    i*=2;
+}
+
+i = 1;
+do {
+    console.log('xd');
+} while(i>10);
+
+let fruits=['banana', 'cherry', 'apple', 'pear']
+
+for(let i=0; i<fruits.length; i++){
+    console.log(fruits[i]);
+}
+
+for(const fruit of fruits){
+    console.log(fruit);
+}
